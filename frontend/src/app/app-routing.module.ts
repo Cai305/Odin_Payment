7d58cc643256824586e069456beb96488555893d
Cai305 <34400593+Cai305@users.noreply.github.com>
2020-11-23 06:45:09 +0200

    landing page

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,41 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { LandingComponent } from './components/landing/landing.component';
+import { SigninComponent } from './components/signin/signin.component';
+import { SignupComponent } from './components/signup/signup.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  { 
+    path: '',
+    redirectTo: '/landing', 
+    pathMatch: 'full'
+  },
+
+  { 
+    path: 'landing', 
+    component: LandingComponent,
+    pathMatch: 'full',
+  },
+
+  { 
+    path: 'home', 
+    component: HomeComponent ,
+    pathMatch: 'full',
+  },
+
+  { path: 'singin', 
+    component: SigninComponent ,
+    pathMatch: 'full', 
+  },
+
+  { path: 'singup', 
+    component: SignupComponent ,
+    pathMatch: 'full',
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
